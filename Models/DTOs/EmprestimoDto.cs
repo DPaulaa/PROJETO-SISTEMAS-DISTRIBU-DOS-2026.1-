c502dc1be3315836f1fd6401c674eadf5c8c6406
DPaulaa <bd.pauladias@gmail.com>
2026-06-15 23:24:18 -0300

    teste

---
+++ Models/DTOs/EmprestimoDto.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaRosa.Models.DTOs;
+
+public class EmprestimoRequest
+{
+    [Required(ErrorMessage = "É necessário selecionar um usuário.")]
+    public int UsuarioId { get; set; }
+
+    [Required(ErrorMessage = "É necessário selecionar um livro.")]
+    public int LivroId { get; set; }
+}
+
+public class EmprestimoResponse
+{
+    public int Id { get; set; }
+    public int UsuarioId { get; set; }
+    public string UsuarioNome { get; set; } = string.Empty;
+    public string UsuarioRole { get; set; } = string.Empty;
+    public int LivroId { get; set; }
+    public string LivroTitulo { get; set; } = string.Empty;
+    public DateTime DataEmprestimo { get; set; }
+    public DateTime? DataDevolucao { get; set; }
+    public DateTime? DataDevolucaoPrevista { get; set; }
+}
+
+
